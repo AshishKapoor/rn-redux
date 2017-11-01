9adebc8a4a7ba29c38c3cd564e1f506eb1cb4873
Ashish Kapoor <swiftobjc@gmail.com>
2017-11-02 00:57:10 +0530

    redux works! throws error due to api changes.

--- actions.js
+++ actions.js
@@ -1,9 +1,9 @@
 import {FETCHING_PEOPLE, FETCHING_PEOPLE_SUCCESS, FETCHING_PEOPLE_FAILURE} from './constants'
 
-function fetchPeopleFromAPI() {
+export function fetchPeopleFromAPI() {
     return (dispatch) => {
         dispatch(getPeople())
-        fetch('https://swapi.co.api/people/')
+        fetch('https://swapi.co/api/people/1/')
         .then(response => response.json())
         .then(json => dispatch(getPeopleSuccess(json.results)))
         .catch(error => dispatch(getPeopleFailure(error)))
